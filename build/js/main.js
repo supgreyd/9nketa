c586b5194c4e2d0b1290ea1b35e847885637cdcc
supgreyd <supgreyd@gmail.com>
2017-08-15 13:45:22 +0300

    eazy done

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,135 @@
+var my_level = 59
+
 $(document).ready(function() {
 
     // Если в проекте используются встроенные js-плагины от Foundation, разкомментировать
-    // $(document).foundation();
+    // $(document).foundation()
+
+  $({someValue: 0}).animate({someValue: my_level}, {
+    duration: 3000,
+    easing:'swing', // can be anything
+    step: function() { // called on every step
+      // Update the element's text with rounded-up value:
+      $('.js-count').text(commaSeparateNumber(Math.round(this.someValue)));
+    }
+  });
+
+  function commaSeparateNumber(val){
+    while (/(\d+)(\d{3})/.test(val.toString())){
+      val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    }
+    return val;
+  }
 
 });
+
+// ---------------------------START_GAUGE_JS-------------------------------------------//
+
+(function() {
+  var Needle, arc, arcEndRad, arcStartRad, barWidth, chart, chartInset, degToRad, el, endPadRad, height, i, margin, needle, numSections, padRad, percToDeg, percToRad, percent, radius, ref, sectionIndx, sectionPerc, startPadRad, svg, totalPercent, width;
+
+  percent = my_level / 100;
+
+  barWidth = 50;
+
+  numSections = 3;
+
+  sectionPerc = 1 / numSections / 2;
+
+  padRad = 0.15;
+
+  chartInset = 0;
+
+  totalPercent = .75;
+
+  el = d3.select('.chart-gauge');
+
+  margin = {
+    top: 0,
+    right: 20,
+    bottom: 30,
+    left: 0
+  };
+
+  width = 250;
+
+  height = width;
+
+  radius = Math.min(width, height) / 2;
+
+  percToDeg = function(perc) {
+    return perc * 360;
+  };
+
+  percToRad = function(perc) {
+    return degToRad(percToDeg(perc));
+  };
+
+  degToRad = function(deg) {
+    return deg * Math.PI / 180;
+  };
+
+  svg = el.append('svg').attr('width', width + margin.left + margin.right).attr('height', height + margin.top + margin.bottom);
+
+  chart = svg.append('g').attr('transform', "translate(" + ((width + margin.left) / 2) + ", " + ((height + margin.top) / 2) + ")");
+
+  for (sectionIndx = i = 1, ref = numSections; 1 <= ref ? i <= ref : i >= ref; sectionIndx = 1 <= ref ? ++i : --i) {
+    arcStartRad = percToRad(totalPercent);
+    arcEndRad = arcStartRad + percToRad(sectionPerc);
+    totalPercent += sectionPerc;
+    startPadRad = 0;
+    endPadRad = sectionIndx === numSections ? 0 : padRad / 2;
+    arc = d3.svg.arc().outerRadius(radius - chartInset).innerRadius(radius - chartInset - barWidth).startAngle(arcStartRad + startPadRad).endAngle(arcEndRad - endPadRad);
+    chart.append('path').attr('class', "arc chart-color" + sectionIndx).attr('d', arc);
+  }
+
+  Needle = (function() {
+    function Needle(len, radius1) {
+      this.len = len;
+      this.radius = radius1;
+    }
+
+    Needle.prototype.drawOn = function(el, perc) {
+      el.append('circle').attr('class', 'needle-center').attr('cx', 0).attr('cy', 0).attr('r', this.radius);
+      return el.append('path').attr('class', 'needle').attr('d', this.mkCmd(perc));
+    };
+
+    Needle.prototype.animateOn = function(el, perc) {
+      var self;
+      self = this;
+      return el.transition().delay(500).ease('elastic').duration(3000).selectAll('.needle').tween('progress', function() {
+        return function(percentOfPercent) {
+          var progress;
+          progress = percentOfPercent * perc;
+          return d3.select(this).attr('d', self.mkCmd(progress));
+        };
+      });
+    };
+
+    Needle.prototype.mkCmd = function(perc) {
+      var centerX, centerY, leftX, leftY, rightX, rightY, thetaRad, topX, topY;
+      thetaRad = percToRad(perc / 2);
+      centerX = 0;
+      centerY = 0;
+      topX = centerX - this.len * Math.cos(thetaRad);
+      topY = centerY - this.len * Math.sin(thetaRad);
+      leftX = centerX - this.radius * Math.cos(thetaRad - Math.PI / 2);
+      leftY = centerY - this.radius * Math.sin(thetaRad - Math.PI / 2);
+      rightX = centerX - this.radius * Math.cos(thetaRad + Math.PI / 2);
+      rightY = centerY - this.radius * Math.sin(thetaRad + Math.PI / 2);
+      return "M " + leftX + " " + leftY + " L " + topX + " " + topY + " L " + rightX + " " + rightY;
+    };
+
+    return Needle;
+
+  })();
+
+  needle = new Needle(55, 10);
+
+  needle.drawOn(chart, 0);
+
+  needle.animateOn(chart, percent);
+
+}).call(this);
+
+// -------------------------------------------------END_GAUGE_JS---------------------------//
